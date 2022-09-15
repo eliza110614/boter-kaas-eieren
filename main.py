9b90acdf0c62fdd5affed58bd572655b2eca0f8c
Eliza Oord <101002283+eliza110614@users.noreply.github.com>
2022-09-15 10:37:07 +0000

    I2 - 3.2

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import random
-from bke import EvaluationAgent, start
+from bke import EvaluationAgent, start, can_win
 
 
 class MyRandomAgent(EvaluationAgent):
   def evaluate(self, board, my_symbol, opponent_symbol):
     getal = 1
-    if board[4] == my_symbol:
-      getal = getal +1
+    if can_win(board, opponent_symbol):
+      getal = getal - 1000
     return getal
 
 
 my_random_agent = MyRandomAgent()
-start(player_o=my_random_agent, player_x = my_random_agent)
+start(player_o=my_random_agent)
