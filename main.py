0931231d63c853af39532c07563279919a48ab1d
Eliza Oord <101002283+eliza110614@users.noreply.github.com>
2022-09-24 14:21:51 +0000

    menu met code spel

--- main.py
+++ main.py
@@ -6,17 +6,43 @@ def menu():
   print("[2] - Tegen een domme tegenstander spelen")
   print("[3] - Tegen een slimme tegenstander spelen")
   print("[4] - De tegenstander trainen")
+  print("[0] - Stop met spelen")
 
 menu()
 option = int(input("Kies je spel"))
 
-class MyRandomAgent(EvaluationAgent):
-  def evaluate(self, board, my_symbol, opponent_symbol):
-    getal = 1
-    if can_win(board, opponent_symbol):
-      getal = getal - 1000
-    return getal
+while option != 0:
+  if option == 1:
+    start()
+  elif option == 2:
+    class MyRandomAgent(EvaluationAgent):
+      def evaluate(self, board, my_symbol, opponent_symbol):
+        return random.randint(1, 500)
 
+    my_random_agent = MyRandomAgent()
+    start(player_o=my_random_agent)
+    
+  elif option == 3:
+    class MyRandomAgent(EvaluationAgent):
+      def evaluate(self, board, my_symbol, opponent_symbol):
+        getal = 1
+        if board [4] == my_symbol:
+          getal = getal + 5
+        return getal
 
-my_random_agent = MyRandomAgent()
-start(player_o=my_random_agent)
+    my_random_agent = MyRandomAgent()
+    start(player_o=my_random_agent)
+
+  elif option == 4:
+    class MyRandomAgent(EvaluationAgent):
+      def evaluate(self, board, my_symbol, opponent_symbol):
+        getal = 1
+        if can_win(board, opponent_symbol):
+          getal = getal - 1000
+        return getal
+
+    my_random_agent = MyRandomAgent()
+    start(player_o=my_random_agent)
+
+  else:
+    print("Invalid option")
