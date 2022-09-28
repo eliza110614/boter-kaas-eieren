21d6d56488844ff904de47d7b172d0f5f4d6f423
Eliza Oord <101002283+eliza110614@users.noreply.github.com>
2022-09-28 10:48:30 +0000

    validatie grafiek plotten in menu + agent trainen in menu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-from bke import EvaluationAgent, start, can_win, MLAgent, is_winner, opponent, RandomAgent, train_and_plot
+from bke import EvaluationAgent, start, can_win, MLAgent, is_winner, opponent, RandomAgent, train_and_plot, train, save
 
 #start menu om te kiezen welk spel je wilt spelen
 def menu():
@@ -7,6 +7,7 @@ def menu():
   print("[2] - Tegen een domme tegenstander spelen")
   print("[3] - Tegen een slimme tegenstander spelen")
   print("[4] - De tegenstander trainen")
+  print("[5] - Validatie grafiek plotten")
   print("[0] - Stop met spelen")
   print()
 
@@ -44,9 +45,27 @@ while option != 0:
 
     my_random_agent = MyRandomAgent()
     start(player_o=my_random_agent)
+
+  elif option == 4:
+    class MyAgent(MLAgent):
+      def evaluate(self, board):
+        if is_winner(board, self.symbol):
+            reward = 1
+        elif is_winner(board, opponent[self.symbol]):
+            reward = -1
+        else:
+            reward = 0
+        return reward
+    
+ 
+    my_agent = MyAgent()
+ 
+    train(my_agent, 3000)
+ 
+    save(my_agent, 'MyAgent_3000')
     
   #agent trainen
-  elif option == 4:
+  elif option == 5:
     class MyAgent(MLAgent):
       def evaluate(self, board):
         if is_winner(board, self.symbol):
@@ -69,10 +88,11 @@ while option != 0:
     trainings=100,
     validations=1000)
     
-    #hyperparameters: met het aanpassen van een hyperparameter kan je machine learning-agent       configureren. er zijn 2 verschillende hyperparameters: 
-    #alpha: de leerfactor van de agent. bepaalt de snelheid waarmee de agent nieuwe kennis         adopteert. hoe hoger het getal, hoe hoger de snelheid waarmee de agent zijn oude kennis       zal vervangen met nieuwe kennis
-    #epsilon: de verkenningsfactor van de agent. bepaalt hoe vaak de agent nieuwe dingen zal       proberen. hoe hoger het getal, hoe vaker de agent een random actie probeert ipv iets          bekends.
-    #een hyperparameter is een floating point-getal tussen de 0 en 1. standaard staat de           agent op alpha-1.0 en epsilon=0.1.
+    #hyperparameters: met het aanpassen van een hyperparameter kan je machine learning-agent configureren. er zijn 2 verschillende hyperparameters: 
+    #alpha: de leerfactor van de agent. bepaalt de snelheid waarmee de agent nieuwe kennis adopteert. hoe hoger het getal, hoe hoger de snelheid waarmee de agent zijn oude kennis zal vervangen met nieuwe kennis
+    #epsilon: de verkenningsfactor van de agent. bepaalt hoe vaak de agent nieuwe dingen zal proberen. hoe hoger het getal, hoe vaker de agent een random actie probeert ipv iets bekends.
+    #een hyperparameter is een floating point-getal tussen de 0 en 1. standaard staat de 9agent op alpha-1.0 en epsilon=0.1.
+    
     my_agent = MyAgent(alpha=0.8, epsilon=0.2)
 
   #als er een invalid teken wordt ingevoerd
